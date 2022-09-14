5e792858e94f7b7be65c35131722ea6a9d70db8a
topt4lent <62635115+topt4lent@users.noreply.github.com>
2022-09-14 21:55:56 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 var express = require('express')
 var cors = require('cors')
-const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'top_db'
-});
+
+
 
 var app = express()
 app.use(cors())
@@ -24,35 +20,3 @@ app.get('/', function (req, res, next) {
 
 
 
-
-
-app.get('/user', function (req, res, next) {
-    connection.query(
-      'SELECT * FROM `user_t`',
-      function(err, results, fields) {
-        res.json(results);
-      }
-    );
-  })
-  
-  app.get('/user/:id', function (req, res, next) {
-    const id = req.params.id;
-    connection.query(
-      'SELECT * FROM `user_t` WHERE `u_id` = ?',
-      [id],
-      function(err, results) {
-        res.json(results);
-      }
-    );
-  })
-
-  
-    app.post('/insert_users', function (req, res, next) {
-    connection.query(
-      'INSERT INTO `users`(`fname`, `lname`, `username`, `password`, `avatar`) VALUES (?, ?, ?, ?, ?)',
-      [req.body.fname, req.body.lname, req.body.username, req.body.password, req.body.avatar],
-      function(err, results) {
-        res.json(results);
-      }
-    );
-  })
