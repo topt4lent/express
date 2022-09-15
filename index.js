b57940e696748c7a2762ef6f860fc353bd93913c
topt4lent <62635115+topt4lent@users.noreply.github.com>
2022-09-15 11:01:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ var app = express()
 app.use(cors())
 app.use(express.json())
 
-/*
+
 app.listen(5000, function () {
   console.log('CORS-enabled web server listening on port 5000')
 })
-*/
+
 app.get('/', function (req, res, next) {
   
       res.send('this is  my express api');
